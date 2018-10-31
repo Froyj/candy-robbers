ddb86e7bbaaec4da31d69ab42a706ac9b2a0f8c2
Claire Lebastard <claire_lebatard@hotmail.com>
2018-10-31 10:18:30 +0100

    v1 du cri: se declenche à l'ouverture page accueil

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -3,13 +3,16 @@ import { Button } from 'reactstrap';
 import '../App.css';
 import { Link } from 'react-router-dom';
 import halloweenIllustration from '../icons/halloweenIllustration.jpg'
+import Cri from '../audio/SF-femme.mp3';
 
 const WelcomePage = () => {
+  const cri = new Audio (Cri);
   return (
     <div>
       <img style={{position: 'fixed', height: 'calc(100vh - 90px)', zIndex: '0'}} src={halloweenIllustration} alt="Halloween" />
       <div className="position-bouton">
-        <Button to="/map" tag={Link} size="lg" className="style-bouton" >TOC TOC TOC</Button>
+        <Button 
+        to="/map" tag={Link} size="lg" className="style-bouton" >TOC TOC TOC</Button>
       </div>
     </div>
   );
