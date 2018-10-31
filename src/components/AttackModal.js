65c0148d02b51ead83cb9a0c48b7e57ada93edcb
Maitetxu Darguy <maitetxu.darguy@gmail.com>
2018-10-31 15:20:57 +0100

    modal attack

--- src/components/AttackModal.js
+++ src/components/AttackModal.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../css/Modal.css'
 
 class AttackModal extends Component {
 
@@ -7,12 +8,12 @@ class AttackModal extends Component {
 
     if (attackAlert === 'ok') {
       return (
-        <div className="modal show modal-loot" tabIndex="-1" role="dialog">
+        <div className="modal show modal-loot modal-style text-center" tabIndex="-1" role="dialog">
           <div className="modal-dialog" role="document">
             <div className="modal-content">
               <div className="modal-body text-center">
-                <p>Vous avez bolossé un autre chasseur de bonbons ! Vous avez gagné 200 points et il en a perdu 200 point</p>
-                <button type="button" data-dismiss="modal" onClick={clearAttackAlert}>Fermer</button>
+                <p style={{ fontSize: '25px', marginBottom: '-5px' }}>Vous avez bolossé un autre chasseur de bonbons ! Vous avez gagné 200 points et il en a perdu 200 point</p>
+                <button style={{ fontSize: '17px' }} type="button" data-dismiss="modal" onClick={clearAttackAlert}>Fermer</button>
               </div>
             </div>
           </div>
@@ -22,12 +23,12 @@ class AttackModal extends Component {
 
     if (attackAlert === 'trop loin') {
       return (
-        <div className="modal show modal-loot" tabIndex="-1" role="dialog">
+        <div className="modal show modal-loot modal-style text-center" tabIndex="-1" role="dialog">
           <div className="modal-dialog" role="document">
             <div className="modal-content">
               <div className="modal-body">
-                <p>Vous êtes trop loin pour attaquer ce joueur</p>
-                <button type="button" data-dismiss="modal" onClick={clearAttackAlert}>Fermer</button>
+                <p style={{ fontSize: '25px' }}>Vous êtes trop loin pour attaquer ce joueur</p>
+                <button style={{ fontSize: '17px' }} type="button" data-dismiss="modal" onClick={clearAttackAlert}>Fermer</button>
               </div>
             </div>
           </div>
